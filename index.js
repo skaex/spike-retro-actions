f38533fe86640f30158ea8c706cf04023a2ad8b4
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2021-10-18 10:55:19 +0000

    Update back index.js

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-// const core = require('@actions/core');
-// const github = require('@actions/github');
-//
-// try {
-//   const sprintDurationDays = core.getInput('sprint-duration-days');
-//   const repoToken = core.getInput('repo-token');
-//   const octokit = github.getOctokit(repoToken);
-//   const context = github.context;
-//
-//   console.log(`Hello ${sprintDurationDays}`);
-//   console.log('------------------------------------');
-//   const time = (new Date()).toTimeString();
-//
-//   const frequency = octokit.rest.repos.getCodeFrequencyStats({
-//     ...context.repo
-//   });
-//
-//   console.log('------------------------------------');
-//   console.log(frequency);
-//   console.log('------------------------------------');
-//
-//   core.setOutput("time", time);
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log('------------------------------------');
-//   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
-//
-console.log('Hello');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const sprintDurationDays = core.getInput('sprint-duration-days');
+  const repoToken = core.getInput('repo-token');
+  const octokit = github.getOctokit(repoToken);
+  const context = github.context;
+
+  console.log(`Hello ${sprintDurationDays}`);
+  console.log('------------------------------------');
+  const time = (new Date()).toTimeString();
+
+  const frequency = octokit.rest.repos.getCodeFrequencyStats({
+    ...context.repo
+  });
+
+  console.log('------------------------------------');
+  console.log(frequency);
+  console.log('------------------------------------');
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log('------------------------------------');
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
