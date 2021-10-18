ce5a3bc2ab2a4608640b6ec56d7e6a849dbd3e40
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2021-10-18 11:26:39 +0000

    Tryng

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const sprintDurationDays = core.getInput('sprint-duration-days');
-  const repoToken = core.getInput('repo-token');
-  const octokit = github.getOctokit(repoToken);
-  const context = github.context;
-
-  console.log(`Hello ${sprintDurationDays}`);
-  console.log('------------------------------------');
-  const time = (new Date()).toTimeString();
-
-  const frequency = octokit.rest.repos.getCodeFrequencyStats({
-    ...context.repo
-  });
-
-  console.log('------------------------------------');
-  console.log(frequency);
-  console.log('------------------------------------');
-
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log('------------------------------------');
-  console.log(`The event payload: ${payload}`);
+  // const sprintDurationDays = core.getInput('sprint-duration-days');
+  // const repoToken = core.getInput('repo-token');
+  // const octokit = github.getOctokit(repoToken);
+  // const context = github.context;
+  //
+  // console.log(`Hello ${sprintDurationDays}`);
+  // console.log('------------------------------------');
+  // const time = (new Date()).toTimeString();
+  //
+  // const frequency = octokit.rest.repos.getCodeFrequencyStats({
+  //   ...context.repo
+  // });
+  //
+  // console.log('------------------------------------');
+  // console.log(frequency);
+  // console.log('------------------------------------');
+  //
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log('------------------------------------');
+  // console.log(`The event payload: ${payload}`);
+  console.log('Hello again');
 } catch (error) {
-  core.setFailed(error.message);
+//  core.setFailed(error.message);
 }
 
 
